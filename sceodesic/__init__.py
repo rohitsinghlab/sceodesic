cb5357a90487a16680cc6ad66a69edda7a99e6d0
aprkh <68245961+aprkh@users.noreply.github.com>
2023-11-10 20:02:12 -0500

    added version info: sceodesic.__version__

--- sceodesic/__init__.py
+++ sceodesic/__init__.py
@@ -11,4 +11,5 @@ from .sceo_main import run_sceo
 # helper functions 
 from .helper.condition_volume_normalization import normalize_condition_volume 
 
+__version__ = '0.0.3'
 __all__ = ['main', 'get_cell_cohorts', 'get_locally_variable_genes', 'normalize_condition_volume']
